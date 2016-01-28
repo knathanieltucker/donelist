e9cf096c06e33f8bbc5f440b59dc942b5258c3c1
knathanieltucker <k.nathaniel.tucker@gmail.com>
2016-01-27 20:58:05 -0800

    added deeds collection
    I added the first collection. Deeds. This will be the things done by
    the users. Note: we should add a time created to this.

---
+++ deeds.js
@@ -0,0 +1,51 @@
+Deeds = new Meteor.Collection( 'deeds' );
+
+Deeds.allow({
+  insert: () => false,
+  update: () => false,
+  remove: () => false
+});
+
+Deeds.deny({
+  insert: () => true,
+  update: () => true,
+  remove: () => true
+});
+
+let DeedsSchema = new SimpleSchema({
+  "creatorId": {
+    type: String,
+    label: "The ID of the creator of this deed",
+    optional: true,
+    denyUpdate: true,
+    autoValue: function () {
+      if (this.isInsert)
+        return Meteor.userId();
+    }
+  },
+  "deed": {
+    type: String,
+    optional: false,
+    label: "What you have done"
+  },
+  "shared.users": {
+    type: [String],
+    label: "The list of user ids can view this deed",
+    optional: true,
+    autoValue: function () {
+      if (this.isInsert)
+        return [];
+    }
+  },
+  "shared.groups": {
+    type: [String],
+    label: "The list of group ids can view this deed",
+    optional: true,
+    autoValue: function () {
+      if (this.isInsert)
+        return [];
+    }
+  }
+});
+
+Comments.attachSchema( CommentsSchema );
